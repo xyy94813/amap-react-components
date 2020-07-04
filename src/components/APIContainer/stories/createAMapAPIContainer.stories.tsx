7f6b6d354038a1de39a8c8e3edc70067e49b46d1
RoXoM <xyy94813@sina.com>
2020-07-04 16:35:29 +0800

    chore: storybook witn environment
    get AMap api key from environment variables
    load env by use dotenv-load
    change the stories of createAMapAPIContainer

--- src/components/APIContainer/stories/createAMapAPIContainer.stories.tsx
+++ src/components/APIContainer/stories/createAMapAPIContainer.stories.tsx
@@ -1,10 +1,18 @@
 import React, {
-  useRef, useState, useEffect, useMemo,
+  useRef,
+  useEffect,
 } from 'react';
 import ReactDOM from 'react-dom';
 
 import createAMapAPIContainer from '../createAMapAPIContainer';
 import useAMapAPI, { useAMapAPIValue } from '../../../hooks/useAMapAPI';
+
+const AMAP_API_KEY: string = process.env.STORYBOOK_AMAP_API_KEY || '';
+
+const APIContainer = createAMapAPIContainer({
+  version: '2.0',
+  apiKey: AMAP_API_KEY,
+});
 
 const ChildComponent = () => {
   const { __AMAP__: AMap }: useAMapAPIValue = useAMapAPI();
@@ -23,46 +31,14 @@ const ChildComponent = () => {
       map.destroy();
     };
   }, [AMap]);
-  return <div ref={$container} style={{ width: 800, height: 600 }} />;
+  return <div ref={$container} style={{ height: 600 }} />;
 };
 
-export const NormalUse = () => {
-  const [apiKey, setAPIKey] = useState(null);
-  const $input = useRef(null);
-
-  const APIContainer = useMemo(() => {
-    if (!apiKey) {
-      return null;
-    }
-    return createAMapAPIContainer({
-      version: '2.0',
-      apiKey: apiKey || '',
-    });
-  }, [apiKey]);
-
-  return (
-    <>
-      <input placeholder="Your api key" ref={$input} />
-      &nbsp;
-      <button
-        type="button"
-        onClick={() => {
-          const curInput: any = $input.current || null;
-          if (curInput) {
-            setAPIKey(curInput.value);
-          }
-        }}
-      >
-        confirm
-      </button>
-      {!!APIContainer && (
-        <APIContainer>
-          <ChildComponent />
-        </APIContainer>
-      )}
-    </>
-  );
-};
+export const NormalUse = () => (
+  <APIContainer>
+    <ChildComponent />
+  </APIContainer>
+);
 
 export default {
   title: 'createAMapAPIContainer',
