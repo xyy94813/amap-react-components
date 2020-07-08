778b4909f75036759a606c47e69458ee975172b8
RoXoM <xyy94813@sina.com>
2020-07-08 21:15:05 +0800

    chore: update stories of `createAMapAPIContainer`

--- src/components/AMapAPIContainer/stories/createAMapAPIContainer.stories.tsx
+++ src/components/AMapAPIContainer/stories/createAMapAPIContainer.stories.tsx
@@ -5,7 +5,7 @@ import useAMapAPI, { useAMapAPIValue } from '../../../hooks/useAMapAPI';
 
 const AMAP_API_KEY: string = process.env.STORYBOOK_AMAP_API_KEY || '';
 
-const APIContainer = createAMapAPIContainer({
+const SyncPluginAPIContainer = createAMapAPIContainer({
   version: '2.0',
   apiKey: AMAP_API_KEY,
   plugins: ['ControlBar', 'ToolBar', 'Scale', 'MapType', 'HawkEye'].map(
@@ -13,7 +13,12 @@ const APIContainer = createAMapAPIContainer({
   ),
 });
 
-const MapContentDrawer = () => {
+const WithoutPluginAPIContainer = createAMapAPIContainer({
+  version: '2.0',
+  apiKey: AMAP_API_KEY,
+});
+
+const MapSyncPluginContentDrawer = () => {
   const { __AMAP__: AMap }: useAMapAPIValue = useAMapAPI();
   const $container = useRef(null);
   const $map = useRef(null);
@@ -22,7 +27,7 @@ const MapContentDrawer = () => {
     if (!AMap || !$container.current) {
       return clearEffect;
     }
-    // eslint-disable-next-line react/no-find-dom-node
+
     const map = new AMap.Map($container.current);
     $map.current = map;
 
@@ -41,13 +46,54 @@ const MapContentDrawer = () => {
   return <div ref={$container} style={{ height: 'calc(100vh - 8px * 2)' }} />;
 };
 
-export const NormalUse = () => (
-  <APIContainer>
-    <MapContentDrawer />
-  </APIContainer>
+export const WithSyncPugin = () => (
+  <SyncPluginAPIContainer>
+    <MapSyncPluginContentDrawer />
+  </SyncPluginAPIContainer>
+);
+
+const MapAsyncPluginContentDrawer = () => {
+  const { __AMAP__: AMap }: useAMapAPIValue = useAMapAPI();
+  const $container = useRef(null);
+  const $map = useRef(null);
+  useEffect(() => {
+    let clearEffect;
+    if (!AMap || !$container.current) {
+      return clearEffect;
+    }
+
+    const map = new AMap.Map($container.current);
+    $map.current = map;
+
+    AMap.plugin(
+      ['ControlBar', 'ToolBar', 'Scale', 'MapType', 'HawkEye'].map(
+        (pluginName) => `AMap.${pluginName}`,
+      ),
+      () => {
+        new AMap.ControlBar().addTo(map);
+        new AMap.ToolBar().addTo(map);
+        new AMap.Scale().addTo(map);
+        new AMap.MapType().addTo(map);
+        new AMap.HawkEye().addTo(map);
+      },
+    );
+
+    clearEffect = () => {
+      map.destroy();
+    };
+
+    return clearEffect;
+  }, [AMap]);
+  return <div ref={$container} style={{ height: 'calc(100vh - 8px * 2)' }} />;
+};
+
+export const WithAsyncPugin = () => (
+  <WithoutPluginAPIContainer>
+    <MapAsyncPluginContentDrawer />
+  </WithoutPluginAPIContainer>
 );
 
 export default {
   title: 'createAMapAPIContainer',
-  component: NormalUse,
+  component: WithSyncPugin,
 };
