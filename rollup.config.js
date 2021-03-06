b2dbc6b2cf817389dd7dc40e3c4dee56d813610f
RoXoM <xyy94813@sina.com>
2021-03-06 13:56:35 +0800

    chore: 集成 rollup

---
+++ rollup.config.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+// eslint-disable-next-line import/extensions
+import pkg from './package.json';
+
+const deps = [...Object.keys(pkg.peerDependencies || {}), ...Object.keys(pkg.dependencies || {})];
+
+const external = (id) => deps.includes(id) || /@babel\/runtime\//.test(id);
+
+const inputPath = path.join(__dirname, 'src', 'index.ts');
+
+const OUTPUT_DIR = 'dist';
+
+const getBasicConf = () => ({
+  input: inputPath,
+  external,
+});
+
+const getDistDir = (moduleType) => {
+  switch (moduleType) {
+    case 'cjs':
+      return OUTPUT_DIR;
+    case 'es':
+      return path.join(OUTPUT_DIR, 'es');
+    default:
+      throw Error('Unknown Module Type');
+  }
+};
+
+const getConf = (env, moduleType) => {
+  const distLib = getDistDir(moduleType);
+  const fileName = `${pkg.name}.${env}.js`;
+  const outputPath = path.join(distLib, fileName);
+
+  const isProd = env === 'production';
+
+  const outputDeclaration = moduleType === 'cjs'; // for only declaration once
+
+  const plugins = [
+    typescript({
+      useTsconfigDeclarationDir: outputDeclaration,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: outputDeclaration, // only es format output declaration
+          declarationDir: path.join(OUTPUT_DIR, 'types'),
+        },
+      },
+    }),
+  ];
+
+  if (isProd) {
+    plugins.push(terser());
+  }
+
+  return {
+    ...getBasicConf(),
+    output: {
+      file: outputPath,
+      format: moduleType,
+      sourcemap: true,
+    },
+    plugins,
+  };
+};
+
+export default [
+  getConf('production', 'cjs'),
+  getConf('production', 'es'),
+];
