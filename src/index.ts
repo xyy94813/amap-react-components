4cbaa9339e7310de2bd7796d825a1e7083642a7c
RoXoM <xyy94813@sina.com>
2021-04-12 21:25:08 +0800

    chore: export more type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-export { default as useAMapAPI } from './hooks/useAMapAPI';
+export { default as useAMapAPI, useAMapAPIValue } from './hooks/useAMapAPI';
 export { default as useAMap } from './hooks/useAMap';
 export { default as useAMapContainerBinder } from './hooks/useAMapContainerBinder';
 
 export * from './components/AMapAPIContainer';
-export { AMapMap, AMapMapContextValue, AMapMapContext } from './components/AMapMap';
+export {
+  AMapMap, MapProps, AMapMapContextValue, AMapMapContext,
+} from './components/AMapMap';
 
-export { default as AMapControlBar } from './components/AMapControlBar';
-export { default as AMapHawkEye } from './components/AMapHawkEye';
-export { default as AMapScale } from './components/AMapScale';
-export { default as AMapToolBar } from './components/AMapToolBar';
+export { default as AMapControlBar, AMapControlBarProps } from './components/AMapControlBar';
+export { default as AMapHawkEye, AMapHawkEyeProps } from './components/AMapHawkEye';
+export { default as AMapScale, AMapScaleProps } from './components/AMapScale';
+export { default as AMapToolBar, AMapToolBarProps } from './components/AMapToolBar';
 
-export { default as AMapGeoJSON } from './components/AMapGeoJSON';
+export {
+  default as AMapGeoJSON,
+  AMapGeoJSONGetOverlayCallback,
+  AMapGeoJSONProps,
+} from './components/AMapGeoJSON';
