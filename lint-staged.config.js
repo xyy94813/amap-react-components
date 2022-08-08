97cf2d30ddefdccd63b817be1689e3206cb8fd2c
RoXoM <xyy94813@sina.com>
2022-08-08 11:16:51 +0800

    chore: bump lint-staged to v13

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  '*.{md,mdx,xml,html,json,yaml,graphql}': 'prettier --write --ignore-unknown',
   '*.{ts,tsx,js,jsx}': [
     'npx eslint -c .eslintrc.js "src/**/*.{ts,tsx,js,jsx}" --fix',
     'git add',
