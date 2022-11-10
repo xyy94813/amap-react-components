1e075b151f2caf8a800f5e46d80edcf821dc1837
RoXoM <xyy94813@sina.com>
2022-11-10 16:25:30 +0800

    chore: 合并 AMap 和 AMapUI 的类型声明

--- src/@types/AMap.d.ts
+++ src/@types/AMap.d.ts
@@ -70,4 +70,26 @@ declare global {
       toGeoJSON: () => GeoJSON.GeoJSON;
     }
   }
+
+  namespace AMapUI {
+    export type SimpleMarkerOptions = Omit<AMap.MarkerOptions, 'content'> & {
+      iconTheme?: string;
+      iconStyle?: string | object;
+      iconLabel?: string | object;
+      showPositionPoint?: boolean | object;
+      containerClassNames?: string;
+    };
+
+    export class SimpleMarker extends AMap.Marker {
+      constructor(opts?: SimpleMarkerOptions);
+      setIconStyle(iconStyle:string):void;
+      setIconThemeAndStyle(iconTheme:string, iconStyle:string):void;
+      setIconLabel(iconLabel:string | object): void;
+      setContainerClassNames(containerClassNames:string): void;
+      showPositionPoint(): void;
+      hidePositionPoint(): void;
+    }
+
+    export const loadUI: (plugins: string | string[], cb: any) => void;
+  }
 }
