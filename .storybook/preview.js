ae26c24c13b5b7554a57c9ce312c20d9a35a46dc
RoXoM <xyy94813@sina.com>
2023-06-05 16:51:01 +0800

    fix: 第一打开 storybook 时，缺少 Suspense 的问题

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
-export const decorators = [];
+import React, { Suspense } from 'react';
+
+export const decorators = [
+  (renderStory, context) => (
+    <Suspense fallback="loading">{renderStory({}, context)}</Suspense>
+  ),
+];
 export const parameters = {};
 export const globalTypes = {};
