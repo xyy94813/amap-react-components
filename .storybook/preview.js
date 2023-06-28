c6fcd2110349340e6eb23f5fa8c253302460569a
RoXoM <xyy94813@sina.com>
2023-06-28 23:12:29 +0800

    chore: 指定 storybook 的菜单顺序

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 
 export const decorators = [
   (renderStory, context) => (
     <Suspense fallback="loading">{renderStory({}, context)}</Suspense>
   ),
 ];
-export const parameters = {};
+export const parameters = {
+  options: {
+    storySort: {
+      order: [
+        "组件(Components)",
+        [
+          "基础组件(Basic)",
+          "地图控件(Control)",
+          "覆盖物(Overlay)",
+          "工具(Tools)",
+        ],
+      ],
+    },
+  },
+};
 export const globalTypes = {};
