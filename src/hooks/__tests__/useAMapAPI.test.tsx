9fc94b433068145b07786f4d10834ac305eb6697
RoXoM <xyy94813@sina.com>
2023-09-26 23:32:45 +0800

    test: 补充 useAMapAPI 单元测试

---
+++ src/hooks/__tests__/useAMapAPI.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { cleanup } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { AMapAPIContext } from '../../components/AMapAPIContainer';
+import useAMapAPI from '../useAMapAPI';
+
+describe('useAMapAPI', () => {
+  beforeEach(cleanup);
+
+  test('should return the AMap API from context', () => {
+    const mockAMapAPIContextValue = {
+      /* mock AMap API object */
+      __AMAP__: {},
+      __AMAP_UI__: {},
+    };
+
+    const wrapper = ({ children }: { children?: React.ReactNode }) => (
+      <AMapAPIContext.Provider value={mockAMapAPIContextValue as any}>
+        {children}
+      </AMapAPIContext.Provider>
+    );
+
+    const { result } = renderHook(() => useAMapAPI(), { wrapper });
+
+    expect(result.current).toBe(mockAMapAPIContextValue);
+  });
+});
