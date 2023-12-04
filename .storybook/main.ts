e317bc117e576cb46a3f9197e9d14f0c85158647
RoXoM <xyy94813@sina.com>
2023-12-05 01:37:02 +0800

    chore: 更新 storybook 至 7.6.3

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   logLevel: 'debug',
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
   /**
    * storybook with typescript
    * see https://storybook.js.org/docs/configurations/typescript-config/
