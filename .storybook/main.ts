61018e966a5c0ff89985b22e9691ff0c987bcd12
RoXoM <xyy94813@sina.com>
2023-12-05 16:14:15 +0800

    docs: 站点展示变更日志

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   logLevel: 'debug',
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../docs/**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
   /**
    * storybook with typescript
