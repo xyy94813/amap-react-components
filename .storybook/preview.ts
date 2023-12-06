2a2a0bf355adc778ddcac1e05288e2465150f97c
RoXoM <xyy94813@sina.com>
2023-12-06 21:40:37 +0800

    docs: 站点展示项目的快速了解

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,5 @@
-import withSuspense from './decorators/withSuspense';
-import DocsPage from './docsPage';
-
+import withSuspense from "./decorators/withSuspense";
+import DocsPage from "./docsPage";
 
 export const decorators = [withSuspense];
 
@@ -8,20 +7,14 @@ export const parameters = {
   options: {
     storySort: {
       order: [
+        "指南",
+        ["快速了解", "变更日志"],
         "组件(Components)",
-        [
-          "基础组件(Basic)",
-          "地图控件(Control)",
-          "覆盖物(Overlay)",
-          "工具(Tools)",
-        ],
+        ["基础组件(Basic)", "地图控件(Control)", "覆盖物(Overlay)", "工具(Tools)"],
         "Hooks",
-        [
-          "useAMapAPI",
-          "useAMap",
-        ],
-      ],
-    },
+        ["useAMapAPI", "useAMap"]
+      ]
+    }
   },
   docs: {
     page: DocsPage
