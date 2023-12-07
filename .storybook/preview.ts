5497042af1d3b8141ad99bcbf0a106b29ffeae64
RoXoM <xyy94813@sina.com>
2023-12-07 23:52:56 +0800

    docs: 站点新增“参与贡献”章节

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,7 +8,7 @@ export const parameters = {
     storySort: {
       order: [
         "指南",
-        ["快速了解", "变更日志"],
+        ["快速了解", "变更日志", "参与贡献"],
         "组件(Components)",
         ["基础组件(Basic)", "地图控件(Control)", "覆盖物(Overlay)", "工具(Tools)"],
         "Hooks",
