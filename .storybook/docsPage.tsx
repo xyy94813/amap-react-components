5a07e8ac6ffef2174a84d65447e752f86fb24a4f
RoXoM <xyy94813@sina.com>
2024-03-15 18:09:58 +0800

    refactor: 文档页面布局实现基于 DefaultPage

--- .storybook/docsPage.tsx
+++ .storybook/docsPage.tsx
@@ -1,26 +1,7 @@
-import type { FC, ReactElement, PropsWithChildren } from "react";
 import React from "react";
 import { NAVIGATE_URL } from "@storybook/core-events";
-import {
-  // DocsPage as DefaultDocsPage,
-  DocsContext,
-  DocsContextProps,
-  Title,
-  Subtitle,
-  Description,
-  Primary,
-  Controls,
-  // Stories,
-  Heading,
-  useOf,
-  DocsStoryProps,
-  Anchor,
-  Canvas,
-  HeadingProps,
-  HeaderMdx
-} from "@storybook/blocks";
+import { DocsPage as DefaultDocsPage, DocsContext, DocsContextProps } from "@storybook/blocks";
 import { styled } from "@storybook/theming";
-import { H3 } from "@storybook/components";
 
 function navigate(context: DocsContextProps, url: string) {
   context.channel.emit(NAVIGATE_URL, url);
@@ -106,90 +87,6 @@ const PageSidebar = () => {
   );
 };
 
-const Subheading: FC<PropsWithChildren<HeadingProps>> = ({ children, disableAnchor }) => {
-  if (disableAnchor || typeof children !== "string") {
-    return <H3>{children}</H3>;
-  }
-  const tagID = globalThis.encodeURIComponent(children.toLowerCase());
-  return (
-    <HeaderMdx as="h3" id={tagID}>
-      {children}
-    </HeaderMdx>
-  );
-};
-
-const DocsStory: FC<DocsStoryProps> = ({
-  of,
-  expanded = true,
-  withToolbar: withToolbarProp = false,
-  __forceInitialArgs = false,
-  __primary = false
-}) => {
-  const { story } = useOf(of || "story", ["story"]);
-
-  // use withToolbar from parameters or default to true in autodocs
-  const withToolbar = story.parameters.docs?.canvas?.withToolbar ?? withToolbarProp;
-
-  return (
-    <Anchor storyId={story.id}>
-      {expanded && (
-        <>
-          <Subheading>{story.name}</Subheading>
-          <Description of={of} />
-        </>
-      )}
-      <Canvas
-        of={of}
-        withToolbar={withToolbar}
-        story={{ __forceInitialArgs, __primary }}
-        source={{ __forceInitialArgs }}
-      />
-    </Anchor>
-  );
-};
-
-const StyledHeading: typeof Heading = styled(Heading)(({ theme }) => ({
-  fontSize: `${theme.typography.size.s2 - 1}px`,
-  fontWeight: theme.typography.weight.bold,
-  lineHeight: "16px",
-  letterSpacing: "0.35em",
-  textTransform: "uppercase",
-  color: theme.textMutedColor,
-  border: 0,
-  marginBottom: "12px",
-
-  "&:first-of-type": {
-    // specificity issue
-    marginTop: "56px"
-  }
-}));
-
-interface StoriesProps {
-  title?: ReactElement | string;
-  includePrimary?: boolean;
-}
-
-const Stories: FC<StoriesProps> = ({ title = "Stories", includePrimary = true }) => {
-  const { componentStories } = React.useContext(DocsContext);
-
-  let stories = componentStories().filter((story) => !story.parameters?.docs?.disable);
-
-  if (!includePrimary) stories = stories.slice(1);
-
-  if (!stories || stories.length === 0) {
-    return null;
-  }
-  return (
-    <>
-      <StyledHeading>{title}</StyledHeading>
-      {stories.map(
-        (story) =>
-          story && <DocsStory key={story.id} of={story.moduleExport} expanded __forceInitialArgs />
-      )}
-    </>
-  );
-};
-
 const StyledPageContentContainer = styled.div<{
   notOnlyOneStory: boolean;
 }>(({ notOnlyOneStory, theme }) =>
@@ -206,13 +103,7 @@ const DocsPage = () => {
   return (
     <>
       <StyledPageContentContainer notOnlyOneStory={notOnlyOneStory}>
-        <Title />
-        <Subtitle />
-        <Description of="meta" />
-        {!notOnlyOneStory ? <Description of="story" /> : null}
-        <Primary />
-        <Controls />
-        {!notOnlyOneStory ? null : <Stories />}
+        <DefaultDocsPage />
       </StyledPageContentContainer>
       {notOnlyOneStory && <PageSidebar />}
     </>
